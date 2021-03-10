b9b66e8eab89ad2cad1ce702350e58a741f975fe
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-03-11 02:36:25 +0530

    Update spam.py

--- userbot/plugins/spam.py
+++ userbot/plugins/spam.py
@@ -13,7 +13,9 @@ from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 
 from hellbot.utils import admin_cmd, sudo_cmd, edit_or_reply
 from userbot.cmdhelp import CmdHelp
+from hellbot.Config import Config
 
+SUDO_WALA = Config.SUDO_USERS
 
 @bot.on(admin_cmd(pattern="spam (.*)"))
 @bot.on(sudo_cmd(pattern="spam (.*)", allow_sudo=True))
@@ -65,48 +67,32 @@ async def spammer(e):
 @bot.on(admin_cmd(pattern="mspam (.*)"))
 @bot.on(sudo_cmd(pattern="mspam (.*)", allow_sudo=True))
 async def tiny_pic_spam(e):
-
     sender = await e.get_sender()
     me = await e.client.get_me()
-
-    if not sender.id == me.id and not FULL_SUDO:
-
+    if not sender.id == me.id and not SUDO_WALA:
         return await e.reply("`Sorry sudo users cant access this command..`")
-
     try:
-
         await e.delete()
-
     except:
-
         pass
-
     try:
-
         counter = int(e.pattern_match.group(1).split(" ", 1)[0])
-
         reply_message = await e.get_reply_message()
-
         if (
             not reply_message
             or not e.reply_to_msg_id
             or not reply_message.media
             or not reply_message.media
         ):
-
             return await e.edit("```Reply to a pic/sticker/gif/video message```")
-
         message = reply_message.media
-
         for i in range(1, counter):
-
             await e.client.send_file(e.chat_id, message)
-
     except:
-
         return await e.reply(
             f"**Error**\nUsage `!mspam <count> reply to a sticker/gif/photo/video`"
         )
+
 
 CmdHelp("spam").add_command(
   "spam", "<number> <text>", "Sends the text 'X' number of times.", ".spam 99 Hello"
