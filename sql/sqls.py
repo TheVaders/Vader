69cc79e395d9519ec0c091212eb7bf7031c01db6
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-03-10 20:28:45 +0530

    Update sqls.py

--- sql/sqls.py
+++ sql/sqls.py
@@ -16,8 +16,8 @@ def start() -> scoped_session:
 
 
 try:
-    SESSION = start()
     BASE = declarative_base()
+    SESSION = start()
 except AttributeError as e:
     # this is a dirty way for the work-around required for #23
     print(
