4c2e6e5f384a74383f043bbf0fe62cb5508cf5fc
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-03-10 21:16:31 +0530

    Update __init__.py

--- sql/__init__.py
+++ sql/__init__.py
@@ -17,15 +17,6 @@ def start() -> scoped_session:
 
 try:
     BASE = declarative_base()
-except AttributeError as e:
-    # this is a dirty way for the work-around required for #23
-    print(
-        "DB_URI is not configured. Features depending on the database might have issues."
-    )
-    print(str(e))
-
-
-try:
     SESSION = start()
 except AttributeError as e:
     # this is a dirty way for the work-around required for #23
