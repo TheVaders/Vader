b21959e84d9b790e188f593a37b186c3cabd7b2f
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-03-21 22:32:14 +0530

    Update telegraph.py

--- userbot/plugins/telegraph.py
+++ userbot/plugins/telegraph.py
@@ -17,8 +17,8 @@ r = telegraph.create_account(short_name=Config.TELEGRAPH_SHORT_NAME)
 auth_url = r["auth_url"]
 
 
-@bot.on(admin_cmd(pattern="t(m|t) ?(.*)", outgoing=True))
-@bot.on(sudo_cmd(pattern="t(m|t) ?(.*)", allow_sudo=True))
+@bot.on(admin_cmd(pattern=f"t(m|t) ?(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern=f"t(m|t) ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
