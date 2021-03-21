837a0ce63752fafe6ae83c046bf7412b6f57fcaf
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-03-21 22:22:22 +0530

    Update carbon.py

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -37,7 +37,7 @@ async def carbon_api(e):
     url = CARBON.format(code=code, lang=CARBONLANG)
     chrome_options = Options()
     chrome_options.add_argument("--headless")
-    chrome_options.binary_location = Config.CHROME_BIN
+    chrome_options.binary_location = Config.GOOGLE_CHROME_BIN
     chrome_options.add_argument("--window-size=1920x1080")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
