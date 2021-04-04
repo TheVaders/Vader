1897cc1b7aa3b9f1ab23aa05b6de440dc7e3a1e7
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-04-04 14:11:12 +0530

    Create __init__.py

---
+++ userbot/plugins/bot/__init__.py
@@ -0,0 +1,20 @@
+from hellbot import ALIVE_NAME
+
+startdis = """
+**👋 Hello there !!
+🙃 I'm {}'s personal bot. Nice to meet you! 😌**
+""".format(ALIVE_NAME)
+
+
+startena = """
+**👋 Hello there !!
+🙃 I am {}'s personal bot.
+😬 You can reach him through me 😉
+😌 Just send me your message and I'll forward it to him...**
+""".format(ALIVE_NAME)
+
+
+starter = """
+**Hoi!! Welcome back master {}. How you doin' ?
+Choose these options from below 😉**
+""".format(ALIVE_NAME)
