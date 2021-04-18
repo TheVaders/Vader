123d0a630a80ae59b71d37ca9df12f860d41efa1
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-04-18 22:02:07 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -8,7 +8,7 @@ from userbot.Config import Config
 from var import Var
 
 StartTime = time.time()
-hellversion = "2.3.4"
+hellversion = "2.3.5"
 botversion = "0.1"
 
 os.system("pip install --upgrade pip")
